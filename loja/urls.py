02fa89f1904e6adb69eb1d16a8d290f6c0d0d3bc
eduardomarquees <eduardomarques.dev01@gmail.com>
2023-12-19 11:39:50 -0300

    Initial commit

---
+++ loja/urls.py
@@ -0,0 +1,17 @@
+import debug_toolbar
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]+ static(settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
+
+# TODO : Remover debug toolbar
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ] + urlpatterns
